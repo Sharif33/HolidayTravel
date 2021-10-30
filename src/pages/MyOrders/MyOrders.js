ff91382105b5cd960f88c27e84b6418447491504
Sharif33 <sharif.cse.nu@gmail.com>
2021-10-30 19:02:17 +0600

    my orders

--- src/pages/MyOrders/MyOrders.js
+++ src/pages/MyOrders/MyOrders.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 const MyOrders = () => {
+    /* const { user } = useAuth();
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/myOrders/${user?.email}`)
+            .then((res) => res.json())
+            .then((data) => setOrders(data));
+    }, []);
+
+    console.log(orders); */
+
+    let deleteCount = 0;
+    const { user } = useAuth();
+    const [orders, setOrders] = useState([]);
+    const email = user?.email;
+    useEffect(() => {
+        fetch(`http://localhost:5000/myOrders/${email}`)
+            .then((res) => res.json())
+            .then((data) => setOrders(data));
+    }, [email, deleteCount]);
+    // console.log(orders);
+
+    /*     //Delete Part
+        const [products, setProducts] = useState([]);
+        useEffect(() => {
+            fetch('http://localhost:5000/myOrders')
+                .then(res => res.json())
+                .then(data => {
+                    setProducts(data);
+                    console.log(products);
+                })
+        }, [products]);
+    
+        //DELETE AN Products
+        const handleDeleteUser = id => {
+            const url = `http://localhost:5000/myOrders/${id}`
+            fetch(url, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+                        console.log(data);
+                        const remainingProducts = orders.filter(order => order._id !== id);
+                        console.log(remainingProducts);
+                        console.log(products);
+                        setOrders(remainingProducts);
+                    }
+                })
+        } */
+
     return (
-        <div>
-            <h2>This is my order</h2>
+        <div className="container p-5">
+            <h2>This is my order: {orders.length}</h2>
+            <div className="row row-cols-md-4">
+                {
+                    orders?.map(order =>
+
+                        <div key={order._id} className="col">
+                            <h5>{order?.resort}</h5>
+                            <img src={order?.image} alt="" />
+                        </div>
+
+
+                    )
+                }
+
+            </div>
         </div>
     );
 };
--- src/pages/Resorts/SingleResort.js
+++ src/pages/Resorts/SingleResort.js
@@ -1,7 +1,12 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
+import useAuth from '../../hooks/useAuth';
 
 const SingleResort = () => {
+
+    const { user } = useAuth();
 
     const { id } = useParams();
 
@@ -13,29 +18,68 @@ const SingleResort = () => {
             .then(data => setResorts(data))
     }, [id])
 
+    // post resort
+
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+
+        axios.post(`http://localhost:5000/orders`, data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    alert('Reserved successfully. Check My Order');
+                    reset();
+                }
+            })
+    }
+
     return (
         <div>
-            <div className='container py-5 my-5'>
+            <img style={{ height: "30rem" }} className='w-100' src={resorts?.image} alt="" />
+            <div className='container py-5'>
                 <div className="row">
-                    <div className="col-md-6 border border-warning shadow-lg">
-                        <div>
-                            <h3 style={{ color: "#62599F" }}>{resorts?.name}</h3>
-                            <h5 className="text-decoration-underline" style={{ color: "#005562" }}>{resorts?.location}</h5>
-                            <img className='w-100' src={resorts?.image} alt="" />
-                        </div>
-                    </div>
-                    <div className="col-md-6 border p-4">
-                        <div className="">
-                            <h5 className='text-secondary'>{resorts?.rating} <span className="text-warning"><i className="fas fa-star"></i></span><span> ({resorts?.reviews} </span><small className="text-secondary">reviews)
-                            </small> </h5>
-                        </div>
-                        <h2 className="text-info  py-3">Cost: <span className="text-danger fw-bold">{resorts?.cost} BDT</span> <span className="fs-6 ">(taxes and vates included)</span></h2>
+                    <div className="col-md-9">
+                        <h3 style={{ color: "#62599F" }}>{resorts?.name}</h3>
+                        <h5 className="text-decoration-underline" style={{ color: "#005562" }}>{resorts?.location}</h5>
+                        <div className=" d-flex justify-contenet-end align-items-center">
 
+                            <h4 className="text-info w-100 py-3">Cost: <span className="text-danger fw-bold">{resorts?.cost} BDT</span> <span className="fs-6 ">(taxes and vates included)</span></h4>
+                            <h6 className='text-secondary w-100'>{resorts?.rating} <span className="text-warning"><i className="fas fa-star"></i></span><span> ({resorts?.reviews} </span><small className="text-secondary">reviews)
+                            </small> </h6>
+
+                        </div>
                         <p>{resorts?.description}</p>
+                    </div>
+
+                    <div className="col-md-3 ">
+                        <div className="add-resort">
+                            <div className="shadow p-4 bg-custom rounded">
+                                <h3 className="text-light py-2">Reserve this Resort</h3>
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <input defaultValue={resorts?.name} {...register("resort")} />
+                                    <input defaultValue={resorts?.cost} {...register("cost")} />
+
+                                    <input defaultValue={user?.displayName} {...register("name")} />
+                                    <input defaultValue={user?.email} {...register("email", { required: true })} />
+                                    <input placeholder="Address"  {...register("address")} />
+                                    <input placeholder="City and country"  {...register("city")} />
+                                    <input placeholder="Phone number"  {...register("phone")} />
+
+                                    <input defaultValue={resorts?.image} {...register("image")} />
+
+                                    <input className="btn btn-outline-light" type="submit" />
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
+
+
             </div>
+
+
         </div>
     );
 };
